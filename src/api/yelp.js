99a7670d1110070f5aaa7beb5b2121f5a1b229b6
Yi-Jacob <jakeyi00@gmail.com>
2023-03-13 20:29:44 -0700

    added results list

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-require('dotenv').config()
 
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      `Bearer ${process.env.REACT_APP_SECRET_NAME}`
-  }),
+      "Bearer Jw0oIMgpId1HV8x-mogAapr36SVRDSAM00qOEvAmLyxCaOV1I0T6kzJbSvazjA6Q7sNS46uHfHzRzLLAESkHYv3ES50h-sUQwtwvh836OsN-D5UwO6ObMswyxDM6YXYx",
+  },
 });
